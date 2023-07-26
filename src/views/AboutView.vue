7adeb1250348de5b8055dabc04af4acdfca0724d
jk9746987 <jk9746987@gmail.com>
2023-07-26 16:36:40 +0800

    vue3+ts+tailwind+simple-git-hooks模板

---
+++ src/views/AboutView.vue
@@ -0,0 +1,20 @@
+<template>
+    <div class="about">
+        <h1>This is an about page</h1>
+        <button class="bg-red-500 hover:bg-blue-500 text-white p-3">按鈕 1</button>
+        <div class="bg-blue-500 text-white p-4">歡迎使用 Tailwind CSS！ test test</div>
+    </div>
+</template>
+
+<style>
+@import 'tailwindcss/base';
+@import 'tailwindcss/components';
+@import 'tailwindcss/utilities';
+@media (min-width: 1024px) {
+    .about {
+        min-height: 100vh;
+        display: flex;
+        align-items: center;
+    }
+}
+</style>
